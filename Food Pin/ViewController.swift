36f0cc275c2443d3aa5e8675abaa927b4358d23d
Chawan Saeed <chawan.saeed02@gmail.com>
2020-11-08 14:07:28 +0300

    Initial Commit

---
+++ Food Pin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Pin
+//
+//  Created by Chawan Saeed on 11/8/20.
+//  Copyright © 2020 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
